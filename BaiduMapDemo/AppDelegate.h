01b417b9b0d6afbacaf1186bb5e72f13c2b0181b
Lafree317 <lafree317@163.com>
2015-09-28 14:11:35 +0800

    Initial Commit

---
+++ BaiduMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiduMapDemo
+//
+//  Created by huchunyuan on 15/9/28.
+//  Copyright © 2015年 Lafree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiduMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduMapDemo
+//
+//  Created by huchunyuan on 15/9/28.
+//  Copyright © 2015年 Lafree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
